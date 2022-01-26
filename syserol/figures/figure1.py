dd635915baccfa4da03c05973ea03a76b52a0e78
maddie-murphy <murphym21@g.ucla.edu>
2022-01-26 15:07:19 -0800

    add r2x fig

--- syserol/figures/figure1.py
+++ syserol/figures/figure1.py
@@ -9,7 +9,7 @@ from tensorpack import perform_CP
 
 
 def makeFigure():
-    ax, f = getSetup((9, 3), (1, 1))
+    ax, f = getSetup((9, 3), (1, 2))
     comps = np.arange(1, 8)
 
     tFacR2X = np.zeros(comps.shape)
@@ -30,15 +30,23 @@ def makeFigure():
         CPR2X[i] = CP.R2X
         sizeCP[i] = tensor_degFreedom(CP, continuous=False)
     
-    ax[0].set_xscale("log", base=2)
-    ax[0].plot(sizeTfac, 1.0 - tFacR2X, ".", label="Continuous Factorization")
-    ax[0].plot(sizeCP, 1.0 - CPR2X, ".", label="CP Factorization")
-    ax[0].set_ylabel("Normalized Unexplained Variance")
-    ax[0].set_xlabel("Size of Reduced Data")
-    ax[0].set_ylim(bottom=0.0)
-    ax[0].set_xlim(2 ** 8 - 100, 2 ** 11 + 100)
-    ax[0].xaxis.set_major_formatter(ScalarFormatter())
-    ax[0].legend()
+    ax[0].scatter(comps, tFacR2X, s=10)
+    ax[0].set_ylabel("CMTF R2X")
+    ax[0].set_xlabel("Number of Components")
+    ax[0].set_xticks([x for x in comps])
+    ax[0].set_xticklabels([x for x in comps])
+    ax[0].set_ylim(0, 1)
+    ax[0].set_xlim(0.5, np.amax(comps) + 0.5)
+
+    ax[1].set_xscale("log", base=2)
+    ax[1].plot(sizeTfac, 1.0 - tFacR2X, ".", label="Continuous Factorization")
+    ax[1].plot(sizeCP, 1.0 - CPR2X, ".", label="CP Factorization")
+    ax[1].set_ylabel("Normalized Unexplained Variance")
+    ax[1].set_xlabel("Size of Reduced Data")
+    ax[1].set_ylim(bottom=0.0)
+    ax[1].set_xlim(2 ** 8 - 100, 2 ** 11 + 100)
+    ax[1].xaxis.set_major_formatter(ScalarFormatter())
+    ax[1].legend()
 
     subplotLabel(ax)
     return f
