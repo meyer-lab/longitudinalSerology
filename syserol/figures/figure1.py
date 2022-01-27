b60d037e8e0cd69d1e674248bd34cd49d4fd851b
maddie-murphy <murphym21@g.ucla.edu>
2022-01-27 14:03:10 -0800

    change comparison to 3D model

--- syserol/figures/figure1.py
+++ syserol/figures/figure1.py
@@ -1,6 +1,7 @@
 from operator import sub
 import numpy as np
 from .common import getSetup, subplotLabel
+from syserol.tensor3D import Tensor3D, reorient_factors_3D, cp_normalize_3D, sort_factors_3D
 from syserol.COVID import Tensor4D
 from syserol.tensor import calcR2X, perform_CMTF, tensor_degFreedom
 from matplotlib.ticker import ScalarFormatter
@@ -14,9 +15,6 @@ def makeFigure():
 
     tFacR2X = np.zeros(comps.shape)
     sizeTfac = np.zeros(comps.shape)
-    CPR2X = np.zeros(comps.shape)
-    sizeCP = np.zeros(comps.shape)
-
     tensor, _ = Tensor4D()
     
     for i, cc in enumerate(comps):
@@ -25,11 +23,20 @@ def makeFigure():
         tFacR2X[i] = tFac.R2X
         sizeTfac[i] = tensor_degFreedom(tFac)
 
-        # Run factorization with standard CP
-        CP = perform_CP(tensor, cc)
-        CPR2X[i] = CP.R2X
-        sizeCP[i] = tensor_degFreedom(CP, continuous=False)
-    
+    # Run factorization for 3D tensor
+    tensor_3D, _ = Tensor3D()
+    CPfacs = [parafac(tensor_3D, cc, tol=1e-10, n_iter_max=1000,
+                        linesearch=True, orthogonalise=2) for cc in comps]
+    sizeCP = [tensor_degFreedom(f, continuous=False) for f in CPfacs]
+    # Normalize 3D factors
+    CPfacs = [cp_normalize_3D(f) for f in CPfacs]
+    CPfacs = [reorient_factors_3D(f) for f in CPfacs]
+    CPfacs = [sort_factors_3D(f) if i > 0 else f for i,
+                f in enumerate(CPfacs)]
+    # Calculate R2X
+    R2X_3D = np.array([calcR2X(f, tensor_3D, continuous=False) for f in CPfacs])
+
+
     ax[0].scatter(comps, tFacR2X, s=10)
     ax[0].set_ylabel("CMTF R2X")
     ax[0].set_xlabel("Number of Components")
@@ -39,8 +46,8 @@ def makeFigure():
     ax[0].set_xlim(0.5, np.amax(comps) + 0.5)
 
     ax[1].set_xscale("log", base=2)
-    ax[1].plot(sizeTfac, 1.0 - tFacR2X, ".", label="Continuous Factorization")
-    ax[1].plot(sizeCP, 1.0 - CPR2X, ".", label="CP Factorization")
+    ax[1].plot(sizeTfac, 1.0 - tFacR2X, ".", label="4D Continuous Tensor Factorization")
+    ax[1].plot(sizeCP, 1.0 - R2X_3D, ".", label="3D Tensor Factorization")
     ax[1].set_ylabel("Normalized Unexplained Variance")
     ax[1].set_xlabel("Size of Reduced Data")
     ax[1].set_ylim(bottom=0.0)
