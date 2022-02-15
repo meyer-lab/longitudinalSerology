375f7586e298d3e8364a3580114afd5d88a93bf4
maddie-murphy <murphym21@g.ucla.edu>
2022-02-14 17:53:30 -0800

    change initialization, also git ignore file

--- syserol/tensor.py
+++ syserol/tensor.py
@@ -3,15 +3,16 @@ Tensor decomposition methods
 """
 import numpy as np
 from tqdm import tqdm
-from scipy.optimize import minimize, Bounds
-from syserol.COVID import Tensor4D
 import tensorly as tl
+from scipy.optimize import minimize, Bounds
 from scipy.optimize._numdiff import approx_derivative
 from tensorly.cp_tensor import cp_lstsq_grad
 from tensorly.tenalg import khatri_rao
 from tensorpack import initialize_cp
+from tensorly.decomposition import parafac
 from copy import deepcopy
 from .COVID import Tensor4D, dayLabels
+from .tensor3D import Tensor3D
 
 
 tl.set_backend('numpy')
@@ -210,6 +211,10 @@ def perform_CMTF(tOrig=None, r=6, tol=1e-5, maxiter=300):
         tOrig, _ = Tensor4D()
 
     tFac = initialize_cp(tOrig, r)
+    # Special initialization for receptor and antigens mode, from 3D factorization
+    tensor_3D, _ = Tensor3D()
+    CPfac = parafac(tensor_3D, r, tol=1e-10, n_iter_max=1000, linesearch=True, orthogonalise=2)
+    tFac.factors[1], tFac.factors[2] = CPfac.factors[1], CPfac.factors[2]
 
     # Pre-unfold
     unfolded = [tl.unfold(tOrig, i) for i in range(tOrig.ndim)]
