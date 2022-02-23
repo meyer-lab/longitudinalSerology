83522f21df4db5f491e7052be801ed070313447c
Aaron Meyer <ameyer@ucla.edu>
2022-02-23 11:49:59 -0800

    Add simple test

--- syserol/test/test_import.py
+++ syserol/test/test_import.py
@@ -1,11 +1,12 @@
 """
 Unit test file.
 """
-import numpy as np
 from ..COVID import pbsSubtractOriginal, Tensor4D
+from ..kaplonek import kaplonek_4D
 
 
 def test_COVID_import():
     """ Test COVID import functions. """
     pbsSubtractOriginal()
     tensor, _ = Tensor4D()
+    tt, _ = kaplonek_4D()
