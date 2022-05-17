b19d2c13c825994e3f8dfa824e1b9f2eec832fbd
maddie-murphy <murphym21@g.ucla.edu>
2022-05-16 23:09:00 -0700

    make fig labels bigger

--- syserol/figures/figure5.py
+++ syserol/figures/figure5.py
@@ -17,6 +17,10 @@ def makeFigure():
     # view original P curves
     df = viewP(P)
     sns.lineplot(data=df, dashes=False, ax=ax[0])
+    ax[0].tick_params(axis='both', which='major', labelsize=10)
+    ax[0].legend(fontsize='medium')
+    ax[0].set_ylabel("Component Weight", fontsize=11.5)
+    ax[0].set_xlabel("Time (days)", fontsize=11.5)
 
     # add some missingness
     imputeSim(sim_tensor, 0.5)
@@ -27,7 +31,9 @@ def makeFigure():
     lineplot(tFac, days.astype(int), "Time (days)", ax[1])
     ax[1].text(27, .4, f"Corrindex: {round(correlation_index(tFac.factors, sim_factors.factors), 3)}", 
         bbox=dict(boxstyle='square', fc="w", ec="k"))
-    ax[1].set_title(rf"Factorization with 50% of Values Removed")
+    ax[1].set_title(rf"Factorization with 50% of Values Removed", fontsize='x-large')
+    ax[1].tick_params(axis='both', which='major', labelsize=10)
+
 
     np.random.seed(1234)
     noise = np.random.normal(size=sim_tensor.shape)
@@ -39,12 +45,16 @@ def makeFigure():
     lineplot(tFac_noisy, days.astype(int), "Time (days)", ax[2])
     ax[2].text(27, .4, f"Corrindex: {round(correlation_index(tFac_noisy.factors, sim_factors.factors), 3)}", 
         bbox=dict(boxstyle='square', fc="w", ec="k"))
-    ax[2].set_title("Factorization with Noise Added")
+    ax[2].set_title("Factorization with Noise Added", fontsize='x-large')
+    ax[2].tick_params(axis='both', which='major', labelsize=10)
+    
+    
 
+    iters = 1
     # Increase missingness and analyze the correlation index
     missInterval = [.5, .55, .6, .65, .7, .75, .8, .83, .87, .9, .93, .96, .99]
-    corrindex_miss = np.zeros((5, len(missInterval)))
-    for i in range(5):
+    corrindex_miss = np.zeros((iters, len(missInterval)))
+    for i in range(iters):
         for j, miss in enumerate(missInterval):
             sim_tensor, _, sim_factors = generate_simulated()
             imputeSim(sim_tensor, miss)
@@ -55,9 +65,9 @@ def makeFigure():
 
     # Vary noise scale and check correlation index
     scale = np.array([0.1, 1, 10, 100, 1000, 10000])
-    corrindex_noise = np.zeros((5, len(scale)))
+    corrindex_noise = np.zeros((iters, len(scale)))
     np.random.seed()
-    for iter in range(5):
+    for iter in range(iters):
         sim_tensor, _, sim_factors = generate_simulated()
         noise = np.random.normal(size=sim_tensor.shape)
         noise *= np.std(sim_tensor)
@@ -68,16 +78,20 @@ def makeFigure():
 
     print("Noise iters: ", corrindex_noise)
 
-    ax[3].errorbar(missInterval, corrindex_miss.mean(axis=0), corrindex_miss.std(axis=0), linestyle='None', marker='o', ms=3)
-    ax[3].set_ylabel("Correlation Index")
-    ax[3].set_xlabel("Missingness Percentage")
+    ax[3].errorbar(missInterval, corrindex_miss.mean(axis=0), corrindex_miss.std(axis=0), linestyle='None', marker='o', ms=4)
+    ax[3].set_ylabel("Correlation Index", fontsize=11.5)
+    ax[3].set_xlabel("Missingness Percentage", fontsize=11.5)
     ax[3].set_ylim(bottom=0.0)
+    ax[3].tick_params(axis='both', which='major', labelsize=10)
     
-    ax[4].errorbar(scale, corrindex_noise.mean(axis=0), corrindex_noise.std(axis=0), linestyle='None', marker='o', ms=3)
-    ax[4].set_ylabel("Correlation Index")
-    ax[4].set_xlabel("Noise Scalar")
+    
+    ax[4].errorbar(scale, corrindex_noise.mean(axis=0), corrindex_noise.std(axis=0), linestyle='None', marker='o', ms=4)
+    ax[4].set_ylabel("Correlation Index", fontsize=11.5)
+    ax[4].set_xlabel("Noise Scalar", fontsize=11.5)
     ax[4].set_xscale("log")
     ax[4].set_ylim(bottom=0.0)
+    ax[4].tick_params(axis='both', which='major', labelsize=10)
+    
 
 
     subplotLabel(ax)
